9307049ddcc07df5f78146f36ea9a4ff80af1a53
冬冷雨 <3155761783@qq.com>
2026-02-08 16:40:24 +0800

    Create setup.py for package installation configurations

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mina-voice',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
