44a0dd510b43a1080b9fe146a6a1fef8ca4d10e2
冬冷雨 <3155761783@qq.com>
2026-02-08 16:37:17 +0800

    Define package with version, author, and import main classes

---
+++ mina_voice/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.0'  
+__author__ = 'ssq777-k'  
+
+from .speech_recognizer import SpeechRecognizer  
+from .motor_controller import MotorController  
+from .command_processor import CommandProcessor
